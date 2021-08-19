e174f8ad68d1870547f8c06e51ed9ea82aabac0a
fernandomg <fernando.greco@gmail.com>
2021-08-19 19:08:13 -0300

    update gravatars names to something more generic

--- tasks/create-gravatars.js
+++ tasks/create-gravatars.js
@@ -5,6 +5,6 @@ require("@nomiclabs/hardhat-ethers");
 task("create-gravatars", "Adds gravatars for the example contract", async () => {
     // deterministically
     const Gravity = await ethers.getContractAt('GravatarRegistry', '0x5fbdb2315678afecb367f032d93f642f64180aa3');
-    await Gravity.functions.createGravatar('Mateo', 'https://images.unsplash.com/photo-1605745341112-85968b19335b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1951&q=80');
-    await Gravity.functions.createGravatar('Fer', 'https://images.unsplash.com/photo-1625252595576-d1fbbd522453?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')
+    await Gravity.functions.createGravatar('Ship', 'https://images.unsplash.com/photo-1605745341112-85968b19335b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1951&q=80');
+    await Gravity.functions.createGravatar('Landscape', 'https://images.unsplash.com/photo-1625252595576-d1fbbd522453?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')
 });
